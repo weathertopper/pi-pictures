9c6104050811e72d3f43fad53d18c61f6036b39e
weathertopper <nathanweatherly94@gmail.com>
2018-01-26 23:37:18 -0500

    trying bash commands in python

--- camera.py
+++ camera.py
@@ -1,8 +1,10 @@
 from picamera import PiCamera
 from time import sleep
+import subprocess
 
 camera = PiCamera()
 camera.start_preview()
 sleep(5)
 camera.capture('picture.jpg')
 camera.stop_preview()
+subprocess.Popen("mkdir willthiswork")
