ffd6c2c5fb68fd9ba302eea941ad96ad8708d3e6
weathertopper <nathanweatherly94@gmail.com>
2018-01-26 23:30:25 -0500

    who cares

--- camera.py
+++ camera.py
@@ -4,5 +4,5 @@ from time import sleep
 camera = PiCamera()
 camera.start_preview()
 sleep(5)
-camera.capture('./tmp-pics/picture.jpg')
+camera.capture('~/git/pi-pictures/tmp-pics/picture.jpg')
 camera.stop_preview()
